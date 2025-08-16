02bf9f5599764c5d2f7cf49761ec1b7b23ac4968
devbydenis <hello.denisrahmadi@gmail.com>
2025-08-16 11:41:44 +0700

    chore: add eslint and prettier for codfe consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import prettier from "eslint-config-prettier";
+import tailwind from "eslint-plugin-tailwindcss";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // prettier (disable conflicting rules)
+  prettier,
+  // tailwindcss lint
+  {
+    plugins: { tailwind },
+    rules: {
+      ...tailwind.configs.recommended.rules,
+    },
+  },
+  // prettier as eslint plugin (biar auto linting format)
+  {
+    plugins: ["prettier"],
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
